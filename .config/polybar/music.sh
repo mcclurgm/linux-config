5ad8257d9f61601358c6231b226598720180ff5a
mcclurgm <mcgearboulder@gmail.com>
2017-09-04 18:00:01 -0600

    Initial polybar work, added custom modules

--- .config/polybar/music.sh
+++ .config/polybar/music.sh
@@ -7,7 +7,7 @@ exec 2>/dev/null
 if [ "$(playerctl status)" = "Playing" ]; then
   title=`exec playerctl metadata xesam:title`
   artist=`exec playerctl metadata xesam:artist`
-  echo "[$artist] $title"
-else
-#  echo "No song currently playing"
+  echo "$artist • $title"
+# else
+#   echo "No song currently playing"
 fi
